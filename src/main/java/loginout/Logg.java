ead275b90c29cccfcb3b00fd908089ef44b5d532
Chitra <Chitra@ch>
2017-11-28 00:57:03 +0530

    First login commit

---
+++ src/main/java/loginout/Logg.java
@@ -0,0 +1,18 @@
+package loginout;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+@Test
+public class Logg {
+	public void getLog(){
+	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();	
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get("https://jqueryui.com/");
+	driver.manage().window().maximize();
+	driver.findElementByLinkText("Datepicker").click();
+	driver.close();
+}
+}
